d05b8a53cb164dd67a947d19276c0047176a5637
Fabio <heloflyer@hotmail.com>
2019-12-12 18:08:04 -0800

    stable version with 3 templating engines

---
+++ routes/shop.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+
+// const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    console.log('adminData: ', adminData.products);
+    const products = adminData.products;
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));                  // old basic version
+    // res.render('shop', { products: products, pageTitle: 'Shop', path: '/' });    
+    res.render('shop', { 
+            products: products, 
+            pageTitle: 'Shop', 
+            path: '/', 
+            hasProducts: products.length > 0,
+            activeShop: true, 
+            productCSS: true 
+    });                                             // handlebars only
+});
+
+module.exports = router;
